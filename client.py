77f3259f951b42e8492cc7ac62a7759deeb48c21
RileyDS <rileydonovanst@gmail.com>
2025-04-24 16:32:01 -0700

    Added server and client (similar to assignment 6)
    added validation for query in client, added query recognition system in server but needs more work

---
+++ client.py
@@ -0,0 +1,51 @@
+import socket as socket
+
+maxBytesToReceive = 1024
+
+if __name__ == "__main__":
+    
+    TCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create the TCP Socket
+
+    # Prompt and validate IP Address
+    while True:
+        serverIP = input("Enter Server IP Address: ") # Prompt user for IP Address
+        try:
+            socket.inet_aton(serverIP) # Validate IP Address
+            break
+        except socket.error:
+            print("Invalid IP Address")
+
+    # Prompt and Validate Server Port student ID, mac address, 
+    while True:
+        try:
+            serverPort = int(input("Enter Server Port Number: ")) #Prompt user for Port Number of the server
+            if 0 <= serverPort <= 65535:
+                break
+            else:
+                print("Invalid Server Port (Not in Range, 0-65535)")
+        except ValueError:
+            print("Invalid Server Port (Not an Integer)")
+    
+    # Connect to Server
+    TCPSocket.connect((serverIP, serverPort))
+    
+    # Sends message(s) to the server
+    while True:
+        message = input("Please enter your query [1-3]: ")
+        #1. What is the average moisture inside my kitchen fridge in the past three hours?
+        #2. What is the average water consumption per cycle in my smart dishwasher?
+        #3. Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?
+        if message == "1" or message == "2" or message == "3":
+            TCPSocket.send(message.encode()) # Send message to the server
+        elif message.lower() == "quit":
+            break
+        else:
+            print("Sorry, this query cannot be processed. Please try one of the following:\n" \
+            "[1] What is the average moisture inside my kitchen fridge in the past three hours?\n" \
+            "[2] What is the average water consumption per cycle in my smart dishwasher?\n" \
+            "[3] Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?")
+        serverResponse = TCPSocket.recv(maxBytesToReceive) # Receive response from server
+        print("Server response: ", serverResponse.decode())
+    
+    # Close access to the server when finished
+    TCPSocket.close()
---
+++ server.py
@@ -0,0 +1,54 @@
+import socket as socket
+
+maxBytesToReceive = 1024
+
+if __name__ == "__main__":
+    
+    # Create the TCP Socket
+    ServerTCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Prompt and validate IP Address ()
+    while True:
+        IP = input("Enter Server IP Address: ") # Prompt user for IP Address
+        try:
+            socket.inet_aton(IP) # Validate IP Address
+            break
+        except socket.error:
+            print("Invalid IP Address")
+
+    # Prompt and Validate Server Port
+    while True:
+        try:
+            Port = int(input("Enter Port Number: ")) #Prompt user for Port Number of the server
+            if 0 <= Port <= 65535:
+                break
+            else:
+                print("Invalid Server Port (Not in Range, 0-65535)")
+        except ValueError:
+            print("Invalid Server Port (Not an Integer)")
+
+    # Bind the socket to an IP & Port
+    ServerTCPSocket.bind((IP, Port))
+
+    # Listen for connections
+    ServerTCPSocket.listen(5) # 5 = number of clients allowed per queue
+
+    # Accept connection(s)
+    incomingSocket, incomingAddress = ServerTCPSocket.accept() # Saves socket and address of incoming connection
+    print(f"Accepted connection from {incomingAddress}")
+    while True:
+        query = incomingSocket.recv(maxBytesToReceive).decode() # Receive message from incoming connection (client)
+        print(f"Received: {query.lower()}")
+        if query.lower() == "1": #Query 1 triggered
+            response = "Calculate response for query 1"
+        elif query.lower() == "2": #Query 2 triggered
+            response = "Calculate response for query 2"
+        elif query.lower() == "3": #Query 3 triggered
+            response = "Calculate response for query 3"
+        if query.decode().lower() == "quit": #if the client terminates, then an empty message is sent, this is caught to end the connection
+            break
+        print(f"Returning: {response}")
+        incomingSocket.send(response.encode()) # Sends reply to client
+    # Close access to client socket when finished
+    print("Connection Terminated")
+    incomingSocket.close()
