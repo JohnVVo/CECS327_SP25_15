92c891f8415ac97ee25b208497d6ab5e0f6f938f
JohnVVo <3johnvo@gmail.com>
2025-04-29 14:49:17 -0700

    Connectedd to NeonDB

--- server.py
+++ server.py
@@ -1,6 +1,29 @@
 import socket as socket
+import psycopg2
+import psycopg2.extras
+
 
 maxBytesToReceive = 1024
+neonDB_connection_string = f"postgresql://neondb_owner:npg_FqAQjlxe47SM@ep-withered-art-a52mqq69-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
+
+# Connect to NeonDB
+try:
+    db = psycopg2.connect(neonDB_connection_string)
+    cursor = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+except Exception as e:
+    print(f"Error connecting to NeonDB: {e}")
+
+def fetch_from_neonDB(query):
+    try:
+        # Execute the query
+        cursor.execute(query)
+
+        # Find matches to query
+        results = cursor.fetchall()    
+
+        return results
+    except Exception as e:
+        return f"Error executing query: {e}"
 
 if __name__ == "__main__":
     
@@ -32,6 +55,7 @@ if __name__ == "__main__":
 
     # Listen for connections
     ServerTCPSocket.listen(5) # 5 = number of clients allowed per queue
+    print("Server listening on Port", Port)
 
     # Accept connection(s)
     incomingSocket, incomingAddress = ServerTCPSocket.accept() # Saves socket and address of incoming connection
@@ -48,7 +72,18 @@ if __name__ == "__main__":
         elif query.lower() == "quit": #if the client terminates, then an empty message is sent, this is caught to end the connection
             break
         print(f"Returning: {response}")
+        res = fetch_from_neonDB(
+            '''
+            SELECT * FROM "IOT_virtual" 
+            ORDER BY id DESC 
+            LIMIT 5;
+            ''')
+        for row in res:
+            print(row['topic'], row['payload'])
         incomingSocket.send(response.encode()) # Sends reply to client
     # Close access to client socket when finished
     print("Connection Terminated")
     incomingSocket.close()
+    # Close the connection to NeonDB
+    cursor.close()
+    db.close()
